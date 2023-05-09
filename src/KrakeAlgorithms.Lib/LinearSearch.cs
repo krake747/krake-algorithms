67664b5c8ea8ff68beb1f03f74a459b96e4b350a
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-05-09 15:48:50 +0200

    ✨ (search): Linear Search Algo
    solved

--- src/KrakeAlgorithms.Lib/LinearSearch.cs
+++ src/KrakeAlgorithms.Lib/LinearSearch.cs
@@ -1,6 +1,20 @@
-﻿namespace KrakeAlgorithms.Lib;
+﻿using System.Numerics;
 
-public class Class1
+namespace KrakeAlgorithms.Lib;
+
+public static class SearchAlgorithms
 {
+    public static bool LinearSearch<T>(IList<T> haystack, T needle)
+        where T : INumber<T>
+    {
+        for (var i = 0; i < haystack.Count; i++)
+        {
+            if (haystack[i] == needle)
+            {
+                return true;
+            }
+        }
 
+        return false;
+    }
 }
