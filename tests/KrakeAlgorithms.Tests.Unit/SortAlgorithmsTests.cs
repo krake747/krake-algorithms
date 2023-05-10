1216e85c95f92398f260658e402b0ac1d4e6346b
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-05-10 15:36:53 +0200

    ✅ (sort): Add passing tests for bubble sort

---
+++ tests/KrakeAlgorithms.Tests.Unit/SortAlgorithmsTests.cs
@@ -0,0 +1,33 @@
+﻿using KrakeAlgorithms.Lib;
+
+namespace KrakeAlgorithms.Tests.Unit;
+
+public sealed class SortAlgorithmsTests
+{
+    [Theory]
+    [ClassData(typeof(UnsortedData))]
+    public void BubbleSort_ShouldReturnSortedArray_WhenInputArrayIsUnsorted(int[] unsorted, int[] expected)
+    {
+        // Arrange
+        var sut = SortAlgorithms.BubbleSort<int>;
+
+        // Act
+        var result = sut(unsorted);
+
+        // Assert
+        result.Should().Equal(expected)
+            .And.BeInAscendingOrder();
+    }
+}
+
+public sealed class UnsortedData : TheoryData<int[], int[]>
+{
+    private readonly int[] _unsorted = { 69, 100, 47, 70, 13, 49, 62, 1, 31, 2 };
+    private readonly int[] _sorted = { 1, 2, 13, 31, 47, 49, 62, 69, 70, 100 };
+    
+    public UnsortedData()
+    {
+        Add(_unsorted, _sorted);
+    }
+}
+
