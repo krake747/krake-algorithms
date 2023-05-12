d3beb5bf852f214484b3e52da7274996bdb5b7e7
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-05-12 16:21:51 +0200

    ✨ (data-structures): Add implementation of a queue

---
+++ src/KrakeAlgorithms.Lib/DataStructures/MyQueue.cs
@@ -0,0 +1,70 @@
+﻿namespace KrakeAlgorithms.Lib.DataStructures;
+
+public sealed class MyQueue<T> : IMyQueue<T>
+{
+    private Node<T>? _head;
+    private Node<T>? _tail;
+    public int Length { get; set; }
+
+    public MyQueue()
+    {
+    }
+    
+    public void Enqueue(T item)
+    {
+        var node = new Node<T>
+        {
+            Value = item
+        };
+
+        Length++;
+        if (_tail is null)
+        {
+            _head = node;
+            _tail = node;
+            return;
+        }
+
+        _tail.Next = node;
+        _tail = node;
+    }
+
+    public T? Dequeue()
+    {
+        if (_head is null)
+        {
+            return default;
+        }
+
+        Length--;
+        var head = _head;
+        _head = _head.Next;
+
+        if (Length is 0)
+        {
+            _tail = default;
+        }
+
+        return head.Value;
+    }
+
+    public T? Peek()
+    {
+        return _head is not null ? _head.Value : default;
+    }
+}
+
+public sealed class Node<T>
+{
+    public T? Value { get; init; }
+    public Node<T>? Next { get; set; }
+}
+
+public interface IMyQueue<T>
+{
+    int Length { get; set; }
+    void Enqueue(T item);
+    T? Dequeue();
+    T? Peek();
+}
+
---
+++ tests/KrakeAlgorithms.Tests.Unit/DataStructures/MyQueueTests.cs
@@ -0,0 +1,93 @@
+﻿using FluentAssertions.Execution;
+using KrakeAlgorithms.Lib.DataStructures;
+
+namespace KrakeAlgorithms.Tests.Unit.DataStructures;
+
+public sealed class MyQueueTests
+{
+    [Fact]
+    public void Enqueue_ShouldEnqueueOneItem_WhenQueueIsEmpty()
+    {
+        // Arrange
+        var sut = new MyQueue<Item>();
+
+        // Act
+        sut.Enqueue(new Item());
+        var result = sut.Length;
+
+        // Assert
+        result.Should().Be(1);
+    }
+    
+    [Fact]
+    public void Enqueue_ShouldEnqueueOneItems_WhenQueueStartsWithOneItem()
+    {
+        // Arrange
+        var sut = new MyQueue<Item>();
+        sut.Enqueue(new Item());
+
+        // Act
+        sut.Enqueue(new Item());
+        var result = sut.Length;
+
+        // Assert
+        result.Should().Be(2);
+    }
+
+    [Fact]
+    public void Dequeue_ShouldDequeueOneItem_WhenQueueStartsWithThreeItems()
+    {
+        // Arrange
+        var sut = new MyQueue<Item>();
+        sut.Enqueue(new Item(0));
+        sut.Enqueue(new Item(1));
+        sut.Enqueue(new Item(2));
+        
+        // Act
+        var result = sut.Dequeue();
+        
+        // Assert
+        using var scope = new AssertionScope();
+        result.Should().Be(new Item(0));
+        sut.Length.Should().Be(2);
+    }
+
+    [Fact]
+    public void Dequeue_ShouldPushNextItemForward_WhenEnqueuePreviouslyDequeuedItem()
+    {
+        // Arrange
+        var sut = new MyQueue<Item>();
+        sut.Enqueue(new Item(0));
+        sut.Enqueue(new Item(1));
+        
+        // Act
+        var item = sut.Dequeue();
+        sut.Enqueue(item);
+        var result = sut.Peek();
+        
+        // Assert
+        using var scope = new AssertionScope();
+        result.Should().Be(new Item(1));
+        sut.Length.Should().Be(2);
+    }
+
+    [Fact]
+    public void Peek_ShouldReturnFirstItemInQueue_WhenQueueStartsWithTwoItems()
+    {
+        // Arrange
+        var sut = new MyQueue<Item>();
+        sut.Enqueue(new Item(0));
+        sut.Enqueue(new Item(1));
+        
+        // Act
+        var result = sut.Peek();
+
+        // Assert
+        using var scope = new AssertionScope();
+        result.Should().Be(new Item(0));
+        sut.Length.Should().Be(2);
+    }
+    
+
+    private readonly record struct Item(int Value);
+}
